b7c93750746edf421059751621786be945f21fa3
Kelvin Hey <57559711+Kelvin-Hey@users.noreply.github.com>
2020-11-10 15:16:07 -0300

    Create Serie de Taylor usando regra de Horner.c

---
+++ Recursao/Serie de Taylor usando regra de Horner.c
@@ -0,0 +1,21 @@
+/*Série de Taylor usando regra de Horner */
+
+double e(int x, int n) {
+
+  static double s;
+  
+  if (n == 0) {
+    return s;
+  }
+  
+  s = 1 + x * s / n;
+  
+  return e(x, n -1);
+}
+
+int main() {
+  
+  printf("%lf \n", e(2, 10));
+  
+  return 0;
+}
