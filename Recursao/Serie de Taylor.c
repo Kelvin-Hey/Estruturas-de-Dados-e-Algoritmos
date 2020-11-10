3d5c38611f1578bc30e2d285eb56e1f309e606c0
Kelvin Hey <57559711+Kelvin-Hey@users.noreply.github.com>
2020-11-10 15:05:43 -0300

    Create Serie de Taylor.c

---
+++ Recursao/Serie de Taylor.c
@@ -0,0 +1,25 @@
+/* Série de Taylor */
+
+double e(int x, int n) {
+
+  static double p = 1;
+  static double f = 1;
+  double r;
+  
+  if (n == 0) {
+    return 1;
+  }
+  
+  r = e(x, n - 1);
+  p = p * x;
+  f = f * n;
+  
+  return r + p / f;
+}
+
+int main() {
+  
+  printf("%lf /n", e(4,15));
+  
+  return 0;
+}
