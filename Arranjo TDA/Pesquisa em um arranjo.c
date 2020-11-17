6ecbed75ea47a4a6cb5b8da50ce434cc5ce49573
Kelvin Hey <57559711+Kelvin-Hey@users.noreply.github.com>
2020-11-17 09:54:04 -0300

    Create Pesquisa em um arranjo.c

---
+++ Arranjo TDA/Pesquisa em um arranjo.c
@@ -0,0 +1,53 @@
+/* Pesquisa em um arranjo */
+
+#include<stdio.h>
+
+stuct Array {
+
+  int A[10];
+  int size;
+  int length;
+};
+
+void Display(struct Array arr) {
+
+  int i;
+  printf("\nOs Elementos são\n");
+  
+  for (i = 0; i < arr.lenght; i++) {
+    printf("%d ", arr.A[i]);
+  }
+}
+
+void Swap(int *x, int *y) {
+  
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+int LinearSearch(struct Array *arr, int key) {
+
+  int i;
+  
+  for (i = 0; i < arr->length; i++) {
+    if (key == arr-> A[i]) {
+      
+      Swap(&arr->A[i], &arr->[0]);
+      return i;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  
+  struct Array arr1 = {{2, 23, 14, 5, 6, 9, 8, 12}, 10, 8};
+  
+  printf("%d", LinearSearch(&arr1, 14));
+  
+  Display(arr1);
+  
+  return 0;
+}
+
